39c3b4ec8a31cfdf8c50fe55b7e1b14b5e1e8898
chh <chh.cs12@nycu.edu.tw>
2024-06-13 07:52:12 +0000

    finish basic main

--- hw4/tests/calculator_test.js
+++ hw4/tests/calculator_test.js
@@ -12,12 +12,6 @@ test("Test main invalid month1", () => {
     assert.throws(()=>{
         Calculator.main(13, 1, 1, 1, 1)
     }, /invalid month1/);
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(12, 1,s 1, 1, 1);
-    // });
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 1, 1, 1);
-    // });
 });
 
 test("Test main invalid month2", () => {
@@ -27,12 +21,6 @@ test("Test main invalid month2", () => {
     assert.throws(()=>{
         Calculator.main(1, 1, 13, 1, 1)
     }, /invalid month2/);
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 12, 1, 1);
-    // });
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 1, 1, 1);
-    // });
 });
 
 test("Test main invalid day1", () => {
@@ -60,12 +48,6 @@ test("Test main invalid year", () => {
     assert.throws(()=>{
         Calculator.main(1, 1, 1, 1, 10001)
     }, /invalid year/);
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 1, 1, 1);
-    // });
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 1, 1, 10000)
-    // });
 });
 
 // -           if (month1 === month2 && day1 > day2) {
@@ -74,26 +56,11 @@ test("Test main day1 > day2", () => {
     assert.throws(()=>{
         Calculator.main(1, 5, 1, 1, 1)
     }, /day1 must be less than day2 if month1 is equal to month2/);
-    
-    assert.throws(()=>{
-        Calculator.main(1, 5, 1, 5, 1)
-    }, /day1 must be less than day2 if month1 is equal to month2/);
-
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 1, 1, 1);
-    // });
 });
 
 test("Test main month1 > month2", () => {
     assert.throws(()=>{
         Calculator.main(5, 1, 1, 1, 1)
     }, /month1 must be less than month2/);
-
-    assert.throws(()=>{
-        Calculator.main(5, 1, 5, 1, 1)
-    }, /month1 must be less than month2/);
-    // assert.doesNotThrow(() => {
-    //     Calculator.main(1, 1, 1, 1, 1);
-    // });
 });
 
